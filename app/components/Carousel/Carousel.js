5d7ae2b1617a12c359c4ecde715df45a3b3ad4ee
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-06-26 20:04:38 +0200

    feat: add carousel

---
+++ app/components/Carousel/Carousel.js
@@ -0,0 +1,61 @@
+'use client'
+
+import { useRef } from "react";
+//import MovieCard from "../MovieCard/MovieCard";
+import styles from "./carousel.module.scss";
+import {
+	IoIosArrowDroprightCircle,
+	IoIosArrowDropleftCircle,
+} from "react-icons/io";
+
+const Carousel = () => {
+	const carouselRef = useRef();
+	const movieCardRef = useRef();
+
+	const handleArrowClick = (type) => {
+		const movieCardWidth = movieCardRef.current?.offsetWidth;
+
+		const value =
+			type === "left"
+				? carouselRef.current.scrollLeft - movieCardWidth
+				: carouselRef.current.scrollLeft + movieCardWidth;
+
+		carouselRef.current.scroll({
+			top: 0,
+			left: value,
+			behavior: "smooth",
+		});
+	};
+
+	return (
+		<div className={styles.carouselContainer}>
+			<div className={styles.arrowsContainer}>
+				<IoIosArrowDropleftCircle
+					className={styles.genericIcon}
+					onClick={() => {
+						handleArrowClick("left");
+					}}
+				/>
+				<IoIosArrowDroprightCircle
+					className={styles.genericIcon}
+					onClick={() => {
+						handleArrowClick();
+					}}
+				/>
+			</div>
+			<div ref={carouselRef} className={styles.carousel}>
+				{/* {list.map((movie, index) => {
+					return (
+						<MovieCard
+							refProp={movieCardRef}
+							movie={movie}
+							key={index}
+						/>
+					);
+				})} */}
+			</div>
+		</div>
+	);
+};
+
+export default Carousel;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+import Carousel from "./components/Carousel/Carousel";
 import HeroComponent from "./components/HeroComponent/HeroComponent";
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <main >
       <HeroComponent />
+
+      <section className={styles.carouselSection}>
+        <Carousel />
+      </section>
     </main>
   );
 }
